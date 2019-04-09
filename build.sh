f128ae5f66b90f0a2acf4b340973f447fd76cdce
Oliver Fuhrer <ofuhrer@users.noreply.github.com>
2019-04-09 10:29:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # tell user what we are doing
-echo "========= executing $0"
+echo "========= executing $0 on `date`"
 
 # build the docker image
 docker build -t docker_image_build_test .
